04162a581a22f429809dd29aa68349e162303d81
Jean-Marc Desiront <jeanmarc.desiront@gmail.com>
2017-04-12 17:20:42 +0200

    New style and about page modifications

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -14,7 +14,7 @@ var About = (props) => {
         more accurate way than browsers themselves. Just enter the name of your own city inside the right box below.
         Let this application gives you the information you need to know.
       </p>
-      <p>Here are some tools i used:</p>
+        <p>Here are some tools i used:</p>
       <ul>
         <li>
           <a href="https://facebook.github.io/react">React</a> - Have a look on this amazing javascript framework!
